2aee7b5fc8e48fd6a5b409fc14ccab4e927ca809
Ivo Kosir <ivo.kosir93@gmail.com>
2021-01-30 02:44:25 +0100

    Successfull JSON read

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "llvm/ADT/StringMap.h"
+#include "llvm/ADT/StringRef.h"
 #include "llvm/Bitcode/BitcodeWriter.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
@@ -6,9 +8,7 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/SymbolTableListTraits.h"
 #include "llvm/IR/Type.h"
-#include "llvm/IR/ValueSymbolTable.h"
 #include "llvm/IR/Verifier.h"
 #include "llvm/Support/JSON.h"
 #include "llvm/Support/MemoryBuffer.h"
@@ -21,177 +21,201 @@ struct FunctionState
   Module &M;
   IRBuilder<> &B;
   Function *const F;
+  StringMap<Value *> TV;
+  StringMap<BasicBlock *> TBB;
 };
 
-namespace Operation
+FunctionType *generateFunctionType(Type *const ret, Type *const param)
 {
-  enum Binary
+  if (param->isVoidTy())
   {
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    MOD,
-  };
-  enum Other
-  {
-    PHI = MOD + 1,
-    CALL,
-  };
-} // namespace Operation
-
-Value *const generateValue(const json::Value &value, const FunctionState &S)
-{
-  if (const auto &name = value.getAsString())
-  {
-    return S.F->getValueSymbolTable()->lookup(*name);
+    return FunctionType::get(ret, false);
   }
-  else if (const auto &number = value.getAsNumber())
+  return FunctionType::get(ret, {param}, false);
+}
+
+Type *const generateType(const json::Object &object, const StringRef key, IRBuilder<> &B)
+{
+  if (const auto fObject = object.getObject(key))
   {
-    return S.B.getInt32(*number);
+    const auto ret = generateType(*fObject, "ret", B);
+    const auto param = generateType(*fObject, "param", B);
+    generateFunctionType(ret, param);
+  }
+
+  const auto s = *object.getString(key);
+  if (s == "void")
+    return B.getVoidTy();
+  else if (s == "i1")
+    return B.getInt1Ty();
+  else if (s == "i32")
+    return B.getInt32Ty();
+  else if (s == "i8*")
+    return B.getInt8PtrTy();
+
+  return nullptr;
+}
+
+Value *const generateValue(const json::Value &value, FunctionState &S)
+{
+  if (const auto name = value.getAsObject())
+  {
+    return S.TV.lookup(*name->getString("name"));
+  }
+  else if (const auto boolean = value.getAsBoolean())
+  {
+    return S.B.getInt1(*boolean);
+  }
+  else if (const auto integer = value.getAsInteger())
+  {
+    return S.B.getInt32(*integer);
+  }
+  else if (value.getAsNull())
+  {
+    const auto type = PointerType::getUnqual(S.B.getVoidTy());
+    return ConstantPointerNull::get(type);
   }
 
   return nullptr;
 }
 
-int generateOperation(StringRef op)
+BasicBlock *getOrCreateBasicBlock(const StringRef label, FunctionState &S)
 {
-  if (op == "add")
-    return Operation::ADD;
-  else if (op == "sub")
-    return Operation::SUB;
-  else if (op == "mul")
-    return Operation::MUL;
-  else if (op == "div")
-    return Operation::DIV;
-  else if (op == "mod")
-    return Operation::MOD;
-  else if (op == "phi")
-    return Operation::PHI;
-  else if (op == "call")
-    return Operation::CALL;
-
-  return 0;
+  auto found = S.TBB.find(label);
+  if (found == S.TBB.end())
+  {
+    auto newBB = BasicBlock::Create(S.C, label, S.F);
+    S.TBB[label] = newBB;
+    return newBB;
+  }
+  return found->getValue();
 }
 
-void generateBinaryOperation(const json::Object &instruction, const Operation::Binary operation, const FunctionState &S)
+Value *generateBinaryOperation(const json::Object &instruction, const llvm::StringRef name, FunctionState &S)
 {
-  const auto name = instruction.getString("name").getValueOr("");
+  const auto op = *instruction.getString("op");
   const auto lhs = generateValue(*instruction.get("lhs"), S);
   const auto rhs = generateValue(*instruction.get("rhs"), S);
 
-  switch (operation)
-  {
-  case Operation::ADD:
-    S.B.CreateNSWAdd(lhs, rhs, name);
-    break;
-  case Operation::SUB:
-    S.B.CreateNSWSub(lhs, rhs, name);
-    break;
-  case Operation::MUL:
-    S.B.CreateNSWMul(lhs, rhs, name);
-    break;
-  case Operation::DIV:
-    S.B.CreateSDiv(lhs, rhs, name);
-    break;
-  case Operation::MOD:
-    S.B.CreateSRem(lhs, rhs, name);
-    break;
-  }
+  if (op == "add")
+    return S.B.CreateNSWAdd(lhs, rhs, name);
+  else if (op == "sub")
+    return S.B.CreateNSWSub(lhs, rhs, name);
+  else if (op == "mul")
+    return S.B.CreateNSWMul(lhs, rhs, name);
+  else if (op == "div")
+    return S.B.CreateSDiv(lhs, rhs, name);
+  else if (op == "mod")
+    return S.B.CreateSRem(lhs, rhs, name);
+
+  else if (op == "and")
+    return S.B.CreateAnd(lhs, rhs, name);
+  else if (op == "or")
+    return S.B.CreateOr(lhs, rhs, name);
+  else if (op == "eq")
+    return S.B.CreateICmpEQ(lhs, rhs, name);
+  else if (op == "ne")
+    return S.B.CreateICmpNE(lhs, rhs, name);
+  else if (op == "sgt")
+    return S.B.CreateICmpSGT(lhs, rhs, name);
+  else if (op == "sge")
+    return S.B.CreateICmpSGE(lhs, rhs, name);
+  else if (op == "slt")
+    return S.B.CreateICmpSLT(lhs, rhs, name);
+  else if (op == "sle")
+    return S.B.CreateICmpSLE(lhs, rhs, name);
+  else if (op == "add")
+    return S.B.CreateAdd(lhs, rhs, name);
+  else if (op == "sub")
+    return S.B.CreateSub(lhs, rhs, name);
+  else if (op == "mul")
+    return S.B.CreateMul(lhs, rhs, name);
+  else if (op == "sdiv")
+    return S.B.CreateSDiv(lhs, rhs, name);
+  else if (op == "srem")
+    return S.B.CreateSRem(lhs, rhs, name);
+
+  return nullptr;
 }
 
-void generatePHI(const json::Object &instruction, const FunctionState &S)
+Value *generateCall(const json::Object &instruction, const llvm::StringRef name, FunctionState &S)
 {
-  const auto name = *instruction.getString("name");
-  const auto label1 = *instruction.getString("label1");
-  const auto name1 = *instruction.getString("name1");
-  const auto label2 = *instruction.getString("label2");
-  const auto name2 = *instruction.getString("name2");
+  const auto Caller = S.M.getFunction(*instruction.getObject("caller")->getString("name"));
+  const auto Arg = generateValue(*instruction.get("arg"), S);
 
-  const auto T = S.F->getValueSymbolTable();
-  const auto V1 = T->lookup(name1);
-  const auto BB1 = &*find_if(*S.F, [&label1](auto &BB) { return BB.getName() == label1; });
-  const auto V2 = T->lookup(name2);
-  const auto BB2 = &*find_if(*S.F, [&label2](auto &BB) { return BB.getName() == label2; });
-
-  const auto phi = S.B.CreatePHI(S.B.getInt32Ty(), 2, name);
-  phi->addIncoming(V1, BB1);
-  phi->addIncoming(V2, BB2);
+  return S.B.CreateCall(Caller->getFunctionType(), Caller, {Arg}, name);
 }
 
-void generateCall(const json::Object &instruction, const FunctionState &S)
+Value *generatePHI(const json::Object &instruction, const llvm::StringRef name, FunctionState &S)
 {
-  const auto name = instruction.getString("name").getValueOr("");
-  const auto callee = *instruction.getString("callee");
-  const auto &args = *instruction.getArray("args");
+  const auto thenValue = generateValue(*instruction.get("then"), S);
+  const auto thenBB = getOrCreateBasicBlock(*instruction.getString("then_label"), S);
+  const auto elseValue = generateValue(*instruction.get("else"), S);
+  const auto elseBB = getOrCreateBasicBlock(*instruction.getString("else_label"), S);
 
-  const auto Callee = S.M.getFunction(callee);
+  const auto phi = S.B.CreatePHI(thenValue->getType(), 2, name);
+  phi->addIncoming(thenValue, thenBB);
+  phi->addIncoming(elseValue, elseBB);
 
-  SmallVector<Value *, 0> Args;
-  for (auto &arg : args)
-  {
-    Args.push_back(generateValue(arg, S));
-  }
-
-  S.B.CreateCall(Callee->getFunctionType(), Callee, Args, name);
+  return phi;
 }
 
-void generateInstruction(const json::Object &instruction, const FunctionState &S)
+Value *generateInstruction(const json::Object &instruction, FunctionState &S)
 {
-  const auto op = *instruction.getString("op");
+  const auto tag = *instruction.getString("tag");
+  StringRef name = *instruction.getString("name");
 
-  auto operation = generateOperation(op);
+  Value *I;
+  if (tag == "binary")
+    I = generateBinaryOperation(instruction, name, S);
+  else if (tag == "call")
+    I = generateCall(instruction, name, S);
+  else if (tag == "phi")
+    I = generatePHI(instruction, name, S);
 
-  switch (operation)
-  {
-  case Operation::ADD:
-  case Operation::SUB:
-  case Operation::MUL:
-  case Operation::DIV:
-  case Operation::MOD:
-    generateBinaryOperation(instruction, Operation::Binary(operation), S);
-    break;
-
-  case Operation::PHI:
-    generatePHI(instruction, S);
-    break;
-  case Operation::CALL:
-    generateCall(instruction, S);
-    break;
-  }
+  S.TV[name] = I;
+  return I;
 }
 
-void generateRet(const json::Object &terminator, const FunctionState &S)
+void generateRet(const json::Object &terminator, FunctionState &S)
 {
   const auto value = generateValue(*terminator.get("value"), S);
   S.B.CreateRet(value);
 }
 
-void generateBr(const json::Object &terminator, const FunctionState &S)
+void generateCondBr(const json::Object &terminator, FunctionState &S)
 {
-  const auto Condition = generateValue(*terminator.get("condition"), S);
-  const auto True = BasicBlock::Create(S.C, *terminator.getString("true"));
-  const auto False = BasicBlock::Create(S.C, *terminator.getString("false"));
-  S.B.CreateCondBr(Condition, True, False);
+  const auto Cond = generateValue(*terminator.get("cond"), S);
+  const auto Then = getOrCreateBasicBlock(*terminator.getString("then"), S);
+  const auto Else = getOrCreateBasicBlock(*terminator.getString("else"), S);
+  S.B.CreateCondBr(Cond, Then, Else);
 }
 
-void generateTerminator(const json::Object &terminator, const FunctionState &S)
+void generateBr(const json::Object &terminator, FunctionState &S)
 {
-  const auto name = *terminator.getString("terminator");
+  const auto Label = getOrCreateBasicBlock(*terminator.getString("label"), S);
+  S.B.CreateBr(Label);
+}
 
-  if (name == "ret")
+void generateTerminator(const json::Object &terminator, FunctionState &S)
+{
+  const auto tag = *terminator.getString("tag");
+
+  if (tag == "ret")
     generateRet(terminator, S);
-  else if (name == "br")
+  else if (tag == "cond_br")
+    generateCondBr(terminator, S);
+  else if (tag == "br")
     generateBr(terminator, S);
 }
 
-void generateBasicBlock(const json::Object &basicBlock, const FunctionState &S)
+void generateBasicBlock(const json::Object &basicBlock, FunctionState &S)
 {
-  const auto label = basicBlock.getString("label").getValueOr("");
+  const auto label = *basicBlock.getString("label");
   const auto &instructions = *basicBlock.getArray("instructions");
   const auto &terminator = *basicBlock.getObject("terminator");
 
-  auto BB = BasicBlock::Create(S.C, label, S.F);
+  auto BB = getOrCreateBasicBlock(label, S);
   S.B.SetInsertPoint(BB);
   auto add = S.B.CreateAdd(S.B.getInt32(5), S.B.getInt32(4));
 
@@ -203,27 +227,26 @@ void generateBasicBlock(const json::Object &basicBlock, const FunctionState &S)
   generateTerminator(terminator, S);
 }
 
-void generateFunction(const json::Object &function, Module &M)
+void generateFunction(const json::Object &function, Module &M, IRBuilder<> &B)
 {
-  const auto name = function.getString("name").getValueOr("");
-  const auto &args = *function.getArray("args");
-  const auto &body = *function.getArray("body");
+  const auto name = *function.getString("name");
+  const auto retType = generateType(function, "ret_type", B);
+  const auto param = *function.getString("param");
+  const auto paramType = generateType(function, "param_type", B);
+  const auto &args = *function.getArray("param");
+  const auto &body = *function.getArray("blocks");
 
-  IRBuilder<> B(M.getContext());
-
-  const auto IntT = B.getInt32Ty();
-  const SmallVector<Type *, 0> ArgsType(args.size(), IntT);
-  const auto FType = FunctionType::get(IntT, ArgsType, false);
+  const auto FType = generateFunctionType(retType, paramType);
   const auto F = Function::Create(FType, Function::ExternalLinkage, name, M);
 
-  auto args_i = args.begin();
-  for (auto &Arg : F->args())
-  {
-    Arg.setName(*args_i->getAsString());
-    args_i++;
-  }
+  FunctionState S{M.getContext(), M, B, F};
 
-  const FunctionState S{M.getContext(), M, B, F};
+  if (!F->args().empty())
+  {
+    const auto arg = F->getArg(0);
+    arg->setName(param);
+    S.TV[param] = arg;
+  }
 
   for (auto &bb : body)
   {
@@ -238,10 +261,11 @@ bool generateModule(const json::Object &module)
 
   LLVMContext C;
   Module M(moduleName, C);
+  IRBuilder<> B(M.getContext());
 
   for (auto &f : functions)
   {
-    generateFunction(*f.getAsObject(), M);
+    generateFunction(*f.getAsObject(), M, B);
   }
 
   if (verifyModule(M, &errs()))
